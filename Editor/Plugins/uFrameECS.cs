1940dca8ae45ec6eaa63e88136333c893f3976a5
InvertGames <invertgamestudios@gmail.com>
2015-11-16 23:45:43 -0500

    no message

--- Editor/Plugins/uFrameECS.cs
+++ Editor/Plugins/uFrameECS.cs
@@ -1014,22 +1014,22 @@ namespace Invert.uFrame.ECS
                 foreach (var ca in item.Database.AllOf<CustomActionNode>().ToArray())
                     ca.CodeAction = true;
             }
-            if (item.BuildNumber < 1)
-            {
-                InvertApplication.Log("Updating database.  You should commit changes to any version control.");
-                var systemNodes = item.Database.AllOf<SystemNode>().ToArray();
-                foreach (var sn in systemNodes)
-                {
-                    foreach (var f in sn.GetAllEditableFilesForNode(item.Database.GetSingle<uFrameDatabaseConfig>())
-                        .Where(p => File.Exists(p.FullPathName)))
-                    {
-                        InvertApplication.Log(string.Format("Removing file {0} for recompile.", f.FullPathName));
-                        File.Delete(f.FullPathName);
-                    }
-                }
-                Execute(new SaveAndCompileCommand() {ForceCompileAll = true});
-                item.BuildNumber = 1;
-            }
+            //if (item.BuildNumber < 1)
+            //{
+            //    InvertApplication.Log("Updating database.  You should commit changes to any version control.");
+            //    var systemNodes = item.Database.AllOf<SystemNode>().ToArray();
+            //    foreach (var sn in systemNodes)
+            //    {
+            //        foreach (var f in sn.GetAllEditableFilesForNode(item.Database.GetSingle<uFrameDatabaseConfig>())
+            //            .Where(p => File.Exists(p.FullPathName)))
+            //        {
+            //            InvertApplication.Log(string.Format("Removing file {0} for recompile.", f.FullPathName));
+            //            File.Delete(f.FullPathName);
+            //        }
+            //    }
+            //    Execute(new SaveAndCompileCommand() {ForceCompileAll = true});
+            //    item.BuildNumber = 1;
+            //}
         }
     }
 
